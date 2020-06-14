7cd35dd6d3a0437de968fb4f5f3e4b2c9ac62dc2
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2020-06-14 00:32:56 -0400

    fixes style

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import { useTranslation } from 'react-i18next';
 import styles from './ShoppingCart.module.css';
 
-export default function NavigationBar() {
+function ShoppingCart(props) {
   const { t } = useTranslation();
   return (
     <div className={styles.container}>
-        <span className={styles.text}>{t('navigationBar.cart')}</span> <span className={styles.badge}>3</span>
+        <span className={styles.text}>{t('navigationBar.cart')}</span>
+        <span className={styles.badge}>3</span>
     </div>
   );
 }
+
+ShoppingCart.propTypes = {
+    count: PropTypes.number,
+}
+
+export default ShoppingCart
