6e243a91d5f2d6572f74cfb2577302c37a1a5e73
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2020-06-14 00:34:47 -0400

    adds count

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -4,13 +4,14 @@ import { useTranslation } from 'react-i18next';
 import styles from './ShoppingCart.module.css';
 
 function ShoppingCart(props) {
-  const { t } = useTranslation();
-  return (
-    <div className={styles.container}>
-        <span className={styles.text}>{t('navigationBar.cart')}</span>
-        <span className={styles.badge}>3</span>
-    </div>
-  );
+    const { count } = props
+    const { t } = useTranslation();
+    return (
+        <div className={styles.container}>
+            <span className={styles.text}>{t('navigationBar.cart')}</span>
+            { count && <span className={styles.badge}>{count}</span> }
+        </div>
+    );
 }
 
 ShoppingCart.propTypes = {
