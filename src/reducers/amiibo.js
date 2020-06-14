0dc90f2d422e7db6ece46ff40a0ecfc515f9c5aa
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2020-06-14 02:36:50 -0400

    adds missing information on success

--- src/reducers/amiibo.js
+++ src/reducers/amiibo.js
@@ -2,6 +2,24 @@ import actions from '../actions'
 
 const { fetch } = actions.amiibo.actions;
 
+const fixedPricesRanges = [19990, 29990, 39990]
+
+const inferPriceFromName = (name) => {
+    const range = Math.floor(name.length / 10)
+    if (range < 3) {
+        return fixedPricesRanges[range]
+    }
+    return fixedPricesRanges[2]
+}
+
+const addMissingInformationToAmiibos = (amiibos) => {
+    return amiibos.map(amiibo => ({ 
+        ...amiibo, 
+        price: inferPriceFromName(amiibo.name), 
+        id: `${amiibo.head}${amiibo.tail}` 
+    }))
+};
+
 const initialState = {
     amiibos: [],
     isLoading: false,
@@ -14,7 +32,7 @@ export default (state = initialState, action) => {
             return { ...state, isLoading: true };
         case fetch.success:
             const { response } = action;
-            return { ...state, amiibos: response, isLoading: false, error: undefined };
+            return { ...state, amiibos: addMissingInformationToAmiibos(response), isLoading: false, error: undefined };
         case fetch.failure:
             const { error } = action;
             return { ...state, isLoading: false, error };
